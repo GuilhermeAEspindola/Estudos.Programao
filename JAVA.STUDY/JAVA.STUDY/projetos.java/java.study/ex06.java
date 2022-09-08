a8c21afe3d9ca64d497b2625b97b87cebbc6c171
Guilherme Augusto Espindola <113059211+GuilhermeAEspindola@users.noreply.github.com>
2022-09-08 20:30:11 -0300

    Update ex06.java

---
+++ JAVA.STUDY/JAVA.STUDY/projetos.java/java.study/ex06.java
@@ -0,0 +1,15 @@
+public class ex06 {
+    public static void main(String[] args) {
+        String resposta = "sim";
+        switch(resposta) {
+            case "sim":
+            System.out.println("resposta " + resposta);
+            case "não":
+            System.out.println("resposta " + resposta);
+            break;
+            default:
+            System.out.println("resposta invalida");
+
+        }
+    }
+}
