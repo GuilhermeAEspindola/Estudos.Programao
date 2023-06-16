92a51ecd37ade63cf92e78b50ac7ec597b5bd327
Guilherme Augusto Espindola <113059211+GuilhermeAEspindola@users.noreply.github.com>
2023-06-16 06:20:07 -0300

    Create ex002.py

---
+++ PYTHON.STUDY/ex04_condiçoes.py/ex002.py
@@ -0,0 +1,6 @@
+nome = str(input("digite seu nome: "))
+if nome ==  "guilherme":
+    print(" que nome de cachorrão em ! ")
+else:
+    print(" que nome bonito! ")
+print(" bom dia, GUERREIRO! {} ".format (nome))
